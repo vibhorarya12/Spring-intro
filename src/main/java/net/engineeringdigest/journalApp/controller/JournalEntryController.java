e6a36c916b030c1247b588331d767249eb255a37
Vibhor Arya <135149764+vibhorarya12@users.noreply.github.com>
2024-09-11 23:54:57 +0530

    controllers created

---
+++ src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
@@ -0,0 +1,16 @@
+package net.engineeringdigest.journalApp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryController {
+
+    @GetMapping("students")
+  public  String Employees(){
+       return "these are all students";
+  }
+
+}
