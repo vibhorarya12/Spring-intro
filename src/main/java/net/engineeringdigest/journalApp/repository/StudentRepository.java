eee16b397869f12a4db76416591b70ebdc475175
Vibhor Arya <135149764+vibhorarya12@users.noreply.github.com>
2024-09-12 23:43:23 +0530

    students repositry created

---
+++ src/main/java/net/engineeringdigest/journalApp/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package net.engineeringdigest.journalApp.repository;
+
+import net.engineeringdigest.journalApp.entity.Students;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Students, Long> {
+}
