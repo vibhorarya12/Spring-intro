0ec0ed51a0e8813d0c088ed95478ef69808eee99
Vibhor Arya <135149764+vibhorarya12@users.noreply.github.com>
2024-09-12 13:14:10 +0530

    basic CRUD done

--- src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
+++ src/main/java/net/engineeringdigest/journalApp/controller/JournalEntryController.java
@@ -3,18 +3,32 @@ package net.engineeringdigest.journalApp.controller;
 import net.engineeringdigest.journalApp.entries.Employees;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+
 @RestController
 @RequestMapping("/journal")
 public class JournalEntryController {
 
-    @GetMapping("Employees")
-  public  String Employees(){
-       return "these are all employees";
-  }
+    private HashMap<String, Employees> entries = new HashMap<>();
+    // register an employee
+    @PostMapping("register")
+    public  boolean register (@RequestBody Employees emp){
+        entries.put(emp.getId(), emp);
+        return true;
 
-  @PostMapping("set-employees")
-  public Employees  register (@RequestBody Employees emp){
-        return  emp;
-  }
+    }
+
+    // get all employees //
+    @GetMapping("getAll")
+    public HashMap<String, Employees> getAll() {
+        return entries;
+    }
+
+    // get employee by id //
+    @GetMapping("id/{empID}")
+    public  Employees getById(@PathVariable Long  empID){
+          return  entries.get(empID.toString());
+    }
+
 
 }
