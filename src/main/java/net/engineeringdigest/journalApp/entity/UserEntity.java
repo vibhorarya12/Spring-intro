5164eef39df2892a983d49baf7e7414211442f79
Vibhor Arya <135149764+vibhorarya12@users.noreply.github.com>
2024-09-14 00:09:10 +0530

    many to many table relation

--- src/main/java/net/engineeringdigest/journalApp/entity/UserEntity.java
+++ src/main/java/net/engineeringdigest/journalApp/entity/UserEntity.java
@@ -5,6 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -17,4 +19,11 @@ public class UserEntity {
     private int id;
     private  String username;
     private  String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private List<Roles> roles = new ArrayList<>();
+
+
 }
