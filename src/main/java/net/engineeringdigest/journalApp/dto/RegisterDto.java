53453ce99ae34374afd4b2d5994aafae7bd9f49c
Vibhor Arya <135149764+vibhorarya12@users.noreply.github.com>
2024-09-14 20:35:40 +0530

    dto added

---
+++ src/main/java/net/engineeringdigest/journalApp/dto/RegisterDto.java
@@ -0,0 +1,26 @@
+package net.engineeringdigest.journalApp.dto;
+
+
+import lombok.Data;
+
+
+public class RegisterDto {
+    private  String username;
+    private  String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
