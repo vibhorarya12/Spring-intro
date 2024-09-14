1811190d3ca0aa7d63e410ac3d3e89003f8e95e7
Vibhor Arya <135149764+vibhorarya12@users.noreply.github.com>
2024-09-15 00:07:33 +0530

    security constants

---
+++ src/main/java/net/engineeringdigest/journalApp/security/SecurityConstants.java
@@ -0,0 +1,7 @@
+package net.engineeringdigest.journalApp.security;
+
+public class SecurityConstants {
+    public  static  final  long JWT_EXPIRATION = 70000;
+    public  static  final  String JTW_SECRET = "XYZ@12CVB";
+
+}
