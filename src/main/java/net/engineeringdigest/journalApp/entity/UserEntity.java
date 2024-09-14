cba057c5130df44c64775209dc18beadf15e41b7
Vibhor Arya <135149764+vibhorarya12@users.noreply.github.com>
2024-09-14 14:20:00 +0530

    getter setter re configured

--- src/main/java/net/engineeringdigest/journalApp/entity/UserEntity.java
+++ src/main/java/net/engineeringdigest/journalApp/entity/UserEntity.java
@@ -20,6 +20,18 @@ public class UserEntity {
     private  String username;
     private  String password;
 
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public List<Roles> getRoles() {
+        return roles;
+    }
+
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
     inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
