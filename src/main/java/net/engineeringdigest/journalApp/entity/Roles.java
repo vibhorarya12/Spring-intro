8a7168e1e2e2e69c924ae9f5baa2113f68a444f3
Vibhor Arya <135149764+vibhorarya12@users.noreply.github.com>
2024-09-14 14:19:46 +0530

    getter setter re configured

--- src/main/java/net/engineeringdigest/journalApp/entity/Roles.java
+++ src/main/java/net/engineeringdigest/journalApp/entity/Roles.java
@@ -6,8 +6,6 @@ import lombok.Setter;
 
 import javax.persistence.*;
 
-@Setter
-@Getter
 @Entity
 @Table(name = "roles")
 public class Roles {
@@ -16,4 +14,25 @@ public class Roles {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
+
+    // Getter for 'id'
+    public int getId() {
+        return id;
+    }
+
+    // Setter for 'id'
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Getter for 'name'
+    public String getName() {
+        return name;
+    }
+
+    // Setter for 'name'
+    public void setName(String name) {
+        this.name = name;
+    }
 }
+
