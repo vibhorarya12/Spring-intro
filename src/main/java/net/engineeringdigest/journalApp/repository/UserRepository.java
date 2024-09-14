292ac6a0a8e6be2dde90d7f5d342c455972dd20b
Vibhor Arya <135149764+vibhorarya12@users.noreply.github.com>
2024-09-14 14:21:26 +0530

    user repository created

---
+++ src/main/java/net/engineeringdigest/journalApp/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package net.engineeringdigest.journalApp.repository;
+
+import net.engineeringdigest.journalApp.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    Optional<UserEntity> findByUsername(String username);
+    Boolean existsByUsername(String username);
+}
