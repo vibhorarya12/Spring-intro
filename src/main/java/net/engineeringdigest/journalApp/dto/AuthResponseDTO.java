7a4b4b5ea8cf8b37b307586cc9f4730a3f3b760a
Vibhor Arya <135149764+vibhorarya12@users.noreply.github.com>
2024-09-15 21:17:36 +0530

    Auth response DTO

---
+++ src/main/java/net/engineeringdigest/journalApp/dto/AuthResponseDTO.java
@@ -0,0 +1,15 @@
+package net.engineeringdigest.journalApp.dto;
+
+import lombok.Data;
+
+@Data
+public class AuthResponseDTO {
+
+    private  String accessToken;
+    private  String tokenType ="Bearer ";
+
+    public AuthResponseDTO(String accessToken) {
+        this.accessToken = accessToken;
+
+    }
+}
