a6e77dbe3af99cce4440914cd846173a82794beb
Vibhor Arya <135149764+vibhorarya12@users.noreply.github.com>
2024-09-15 21:20:28 +0530

    jwt reconfigured

--- src/main/java/net/engineeringdigest/journalApp/security/JwtGenerator.java
+++ src/main/java/net/engineeringdigest/journalApp/security/JwtGenerator.java
@@ -4,47 +4,50 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwt;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
 import java.util.Date;
 
 @Component
 public class JwtGenerator {
-
-    public  String generateToken(Authentication authentication){
+    private static  final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+    public String generateToken(Authentication authentication) {
         String username = authentication.getName();
         Date currentDate = new Date();
         Date expireDate = new Date(currentDate.getTime() + SecurityConstants.JWT_EXPIRATION);
+
         String token = Jwts.builder()
                 .setSubject(username)
-                .setIssuedAt(new Date())
+                .setIssuedAt( new Date())
                 .setExpiration(expireDate)
-                .signWith(SignatureAlgorithm.ES512, SecurityConstants.JTW_SECRET)
+                .signWith(key,SignatureAlgorithm.HS512)
                 .compact();
+        System.out.println("New token :");
+        System.out.println(token);
         return token;
-
     }
-    public  String getUsernameFromJWT(String token){
-        Claims claims = Jwts.parser()
-                .setSigningKey(SecurityConstants.JTW_SECRET)
+    public String getUsernameFromJWT(String token){
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
                 .parseClaimsJws(token)
                 .getBody();
-
-        return  claims.getSubject();
-
+        return claims.getSubject();
     }
 
-    public boolean validateToken(String token){
-
+    public boolean validateToken(String token) {
         try {
-            Jwts.parser()
-                    .setSigningKey(SecurityConstants.JTW_SECRET)
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
                     .parseClaimsJws(token);
-            return  true;
-        } catch (Exception e) {
-            throw new AuthenticationCredentialsNotFoundException("JWT was expired or incorrect");
+            return true;
+        } catch (Exception ex) {
+            throw new AuthenticationCredentialsNotFoundException("JWT was exprired or incorrect",ex.fillInStackTrace());
         }
     }
 }
